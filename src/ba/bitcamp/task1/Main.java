0155994889a60e7c062afb99f5c3f47399be033e
AlenBumbulovic <AlenBumbulovic>
2015-06-15 22:22:57 +0200

    Homework 15

---
+++ src/ba/bitcamp/task1/Main.java
@@ -0,0 +1,20 @@
+package ba.bitcamp.task1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cashbox c = new Cashbox("Moja kasa");
+
+		System.out.println(c.cashboxBalance());
+		c.addMoney(30);
+		System.out.println(c.cashboxBalance());
+		c.addMoney(100);
+		System.out.println(c.cashboxBalance());
+		c.removeAllMoney();
+		System.out.println(c.cashboxBalance());
+
+	}
+
+}
