489f5a829e9262de201679a5a1cb60200d69192d
AlenBumbulovic <AlenBumbulovic>
2015-06-15 22:24:45 +0200

    Change

--- src/ba/bitcamp/task2/Animal.java
+++ src/ba/bitcamp/task2/Animal.java
@@ -1,5 +1,18 @@
 package ba.bitcamp.task2;
 
 public class Animal {
+	
+	private String name;
+	private int active;
+	private int water;
+	
+	public Animal(String name, int active, int water) {
+		super();
+		this.name = name;
+		this.active = active;
+		this.water = water;
+	}
+	
+	
 
 }
